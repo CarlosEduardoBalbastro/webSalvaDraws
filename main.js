ef017aad6eafdeb84b063cbce83d428ebe8bacb1
CarlosEduardoBalbastro <balbastrocae@gmail.com>
2024-05-11 11:12:00 -0300

    continuando web Salvi

--- main.js
+++ main.js
@@ -3,3 +3,15 @@ function mensajeForm(){
 
     mens.innerText = "Thank you for contacting us!";
 }
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const modoOscuroBtn = document.getElementById('modo-oscuro-btn');
+    const body = document.body;
+
+    modoOscuroBtn.addEventListener('click', function () {
+        // Alternar la clase 'dark-mode' en el body
+        body.classList.toggle('dark-mode');
+    });
+});
